cb9bb6d354d588e9d7c3cf2ff5c3c6aa5b81727a
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-09-17 23:46:50 -0300

    simplifica script

--- .github/scripts/telegram.sh
+++ .github/scripts/telegram.sh
@@ -4,19 +4,12 @@
 echo $TELEGRAM_CHAT_ID
 echo $STATUS
 
-exit 0
 # Get the token from Travis environment vars and build the bot URL:
 BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
 
 # Set formatting for the message. Can be either "Markdown" or "HTML"
 PARSE_MODE="Markdown"
 
-# Use built-in Travis variables to check if all previous steps passed:
-if [ $STATUS -ne 0 ]; then
-    build_status="failed"
-else
-    build_status="succeeded"
-fi
 
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
@@ -30,7 +23,7 @@ send_msg () {
 # characters, since they're reserved in bash
 send_msg "
 -------------------------------------
-Github action *${build_status}!*
+Github action *${$STATUS}!*
 \`Repository:  ${GITHUB_REPOSITORY}\`
 \`Branch:      ${GITHUB_REF_NAME}\`
 [Job Log here](${$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID})
